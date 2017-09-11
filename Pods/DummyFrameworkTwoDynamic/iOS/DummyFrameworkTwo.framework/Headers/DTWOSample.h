8d264147ba32dbde438112b11c41cc267db82815
Marc Terns <tenxurz@gmail.com>
2017-09-11 13:34:59 -0500

    Pushing framework six

---
+++ Pods/DummyFrameworkTwoDynamic/iOS/DummyFrameworkTwo.framework/Headers/DTWOSample.h
@@ -0,0 +1,15 @@
+//
+//  DTWOSample.h
+//  DummyFrameworkTwo
+//
+//  Created by Marc Terns on 9/9/17.
+//  Copyright © 2017 Marc Terns. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DTWOSample : NSObject
+
+- (void)print;
+
+@end
