3746aaa36ad0c47f138bc7e792d609a9dc89be7a
Marc Terns <tenxurz@gmail.com>
2017-09-10 21:43:20 -0500

    Adding gitignore

--- Pods/Target Support Files/Pods-ProofOfConcept/Pods-ProofOfConcept-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ProofOfConcept : NSObject
-@end
-@implementation PodsDummy_Pods_ProofOfConcept
-@end
--- ProofOfConcept/ViewController.m
+++ ProofOfConcept/ViewController.m
@@ -10,6 +10,8 @@
 @import DummyFrameworkOne;
 @import DummyFrameworkThree;
 @import DummyFrameworkTwo;
+@import DummyFrameworkFour;
+@import DummyFrameworkFive;
 
 @interface ViewController ()
 
